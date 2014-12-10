79d88ce812e3625543ef3f80f2cc9dfbe8dcf2c2
BSTCloud <carlitoslol@hotmail.es>
2014-12-10 20:11:22 +0100

    Interfaz v3

---
+++ HS_Project/src/com/main/collection_activity.java
@@ -0,0 +1,14 @@
+package com.main;
+
+import com.example.hs_project.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class collection_activity extends Activity {
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.collection_activity);
+       }
+}
--- HS_Project/src/com/main/main_activity.java
+++ HS_Project/src/com/main/main_activity.java
@@ -4,13 +4,27 @@ import com.example.hs_project.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.content.Intent;
 
 public class main_activity extends Activity{
+	
+	private Button collection;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_activity);
+		
+		collection = (Button)findViewById(R.id.Button1);
 	}
-
+	
+	public void onClick(View v){
+		if(v.getId() == R.id.Button1){
+			Intent intent = new Intent(main_activity.this, collection_activity.class);
+			startActivity(intent);
+		}
+	}
 	
 }
