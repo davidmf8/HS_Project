c7d21656d554b53d34450e24c475e8e6de81780a
BSTCloud <carlitoslol@hotmail.es>
2014-12-10 19:11:30 +0100

    Interfaz v2

--- HS_Project/gen/com/example/hs_project/R.java
+++ HS_Project/gen/com/example/hs_project/R.java
@@ -11,10 +11,16 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int button=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int menu=0x7f020002;
     }
     public static final class id {
-        public static final int button1=0x7f060000;
+        public static final int APPBackground=0x7f060000;
+        public static final int BrownBox=0x7f060001;
+        public static final int Button1=0x7f060002;
+        public static final int Button2=0x7f060003;
+        public static final int Button3=0x7f060004;
     }
     public static final class layout {
         public static final int main_activity=0x7f030000;
